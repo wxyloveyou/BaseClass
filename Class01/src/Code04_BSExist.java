fb826c5621c205a3befdf01d3fced14805f8d9e4
WXY <2423144591@qq.com>
2022-08-07 10:19:54 +0800

    二分查找

---
+++ Class01/src/Code04_BSExist.java
@@ -0,0 +1,32 @@
+/**
+ * @author WXY
+ * @create 2022-07-26 22:18
+ */
+public class Code04_BSExist {
+    public static boolean exist(int[] sortedArr,int num) {
+        if (sortedArr == null || sortedArr.length == 0) {
+            return false;
+        }
+        int L = 0;
+        int R = sortedArr.length - 1;
+        int mid = 0;
+        while (L < R){
+            mid = L + ((R - L) >> 1);
+            if (sortedArr[mid] == num) {
+                return true;
+            } else if (sortedArr[mid] > num) {
+                R = mid - 1;
+            } else {
+                L = mid + 1;
+            }
+        }
+        return sortedArr[L] == num;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 2, 4, 6, 8, 1, 3, 5, 7, 9};
+        Code03_InsertionSort.insertionSort(arr);
+        boolean exist = exist(arr, 1);
+        System.out.println(exist);
+    }
+}
