625630aad036dcd7232e033feba45c9b772efaf3
WXY <2423144591@qq.com>
2022-08-12 21:31:42 +0800

    info 派对的最大快乐值

---
+++ class08/src/Code09_MaxHappy.java
@@ -0,0 +1,127 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author : WXY
+ * @create : 2022-08-12 21:05
+ * @Info :  派对的最大快乐值
+ *
+ * 如果一个员工来， 那么他的直接下级就不可以来， 但是直接下级的直接下级可以来，
+ * 每个员工都有一个快乐值，那么怎么发请帖可以得到最大的快乐值
+ *
+ * 员工信息的定义如下：
+ * class Employee{
+ *     public int happy;//这名员工可以带来的快乐值
+ *     List<Employee> subordinates;//这个员工有哪些直接下级
+ * }
+ *
+ *
+ */
+public class Code09_MaxHappy {
+    public static class Employee{
+        public int happy;
+        public List<Employee> nexts;
+
+        public Employee(int h) {
+            happy = h;
+            nexts = new ArrayList<>();
+        }
+    }
+
+    public static int maxHappy1(Employee boss) {
+        if (boss == null) {
+            return 0;
+        }
+        return process1(boss, false);
+    }
+
+    private static int process1(Employee cur, boolean up) {
+        if (up) {
+            int ans = 0;
+            for (Employee next : cur.nexts) {
+                ans += process1(next, false);
+            }
+            return ans;
+        } else {
+            int p1 = cur.happy;
+            int p2 = 0;
+            for (Employee next : cur.nexts) {
+                p1 += process1(next, true);
+                p2 += process1(next, false);
+            }
+            return Math.max(p1, p2);
+        }
+    }
+
+
+    public static class Info {
+        public int yes;
+        public int no;
+
+        public Info(int yes, int no) {
+            this.yes = yes;
+            this.no = no;
+        }
+    }
+    public static int maxHappy2(Employee boss) {
+        if (boss == null) {
+            return 0;
+        }
+        Info all = process2(boss);
+        return Math.max(all.yes, all.no);
+    }
+
+    private static Info process2(Employee x) {
+        if (x.nexts.isEmpty()) {
+            return new Info(x.happy, 0);
+        }
+        int yes = x.happy;
+        int no = 0;
+        for (Employee next : x.nexts) {
+            Info nextInfo = process2(next);
+            yes += nextInfo.no;
+            no += Math.max(nextInfo.yes, nextInfo.no);
+        }
+        return new Info(yes, no);
+    }
+
+    // for test
+    public static Employee genarateBoss(int maxLevel, int maxNexts, int maxHappy) {
+        if (Math.random() < 0.02) {
+            return null;
+        }
+        Employee boss = new Employee((int) (Math.random() * (maxHappy + 1)));
+        genarateNexts(boss, 1, maxLevel, maxNexts, maxHappy);
+        return boss;
+    }
+
+    // for test
+    public static void genarateNexts(Employee e, int level, int maxLevel, int maxNexts, int maxHappy) {
+        if (level > maxLevel) {
+            return;
+        }
+        int nextsSize = (int) (Math.random() * (maxNexts + 1));
+        for (int i = 0; i < nextsSize; i++) {
+            Employee next = new Employee((int) (Math.random() * (maxHappy + 1)));
+            e.nexts.add(next);
+            genarateNexts(next, level + 1, maxLevel, maxNexts, maxHappy);
+        }
+    }
+
+    public static void main(String[] args) {
+        int maxLevel = 4;
+        int maxNexts = 7;
+        int maxHappy = 100;
+        int testTimes = 100000;
+        for (int i = 0; i < testTimes; i++) {
+            Employee boss = genarateBoss(maxLevel, maxNexts, maxHappy);
+            if (maxHappy1(boss) != maxHappy2(boss)) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("finish!");
+    }
+
+
+}
