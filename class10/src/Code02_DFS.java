89fad1a87e8b4a979672c56d7ed49408f299d822
WXY <2423144591@qq.com>
2022-08-14 18:06:38 +0800

    info 深度优先遍历

---
+++ class10/src/Code02_DFS.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+import java.util.InputMismatchException;
+import java.util.Stack;
+
+/**
+ * @author : WXY
+ * @create : 2022-08-14 18:01
+ * @Info : 深度优先遍历
+ */
+public class Code02_DFS {
+    public static void dfs(Node node) {
+        if (node == null) {
+            return;
+        }
+        Stack<Node> stack = new Stack<>();
+        HashSet<Node> set = new HashSet<>();
+        stack.push(node);
+        set.add(node);
+        System.out.println(node.value);
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            for (Node next :
+                    cur.nexts) {
+                if (!set.contains(next)) {
+                    stack.push(cur);
+                    stack.push(next);
+                    set.add(next);
+                    System.out.println(next.value);
+                    break;
+                }
+            }
+        }
+    }
+}
