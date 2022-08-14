532ab1f65ddaa442a6002fcc7524058270d66ecb
WXY <2423144591@qq.com>
2022-08-14 17:17:48 +0800

    info 点结构

---
+++ class10/src/Node.java
@@ -0,0 +1,25 @@
+import com.sun.javafx.geom.Edge;
+
+import java.util.ArrayList;
+
+/**
+ * @author : WXY
+ * @create : 2022-08-14 17:15
+ * @Info : 点结构
+ */
+//点结构的描述
+public class Node {
+    public int value;
+    public int in;
+    public int out;
+    public ArrayList<Node> nexts;
+    public ArrayList<Edge> edges;
+
+    public Node(int value) {
+        this.value = value;
+        in = 0;
+        out = 0;
+        nexts = new ArrayList<>();
+        edges = new ArrayList<>();
+    }
+}
