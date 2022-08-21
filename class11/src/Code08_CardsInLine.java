23f4398bcbaa4792f9a2edfce65915eb36746f6b
24231 <23144591@qq.com>
2022-08-21 22:57:14 +0800

    info Code08_CardsInLine

---
+++ class11/src/Code08_CardsInLine.java
@@ -0,0 +1,15 @@
+/**
+ * @author : WXY
+ * @create : 2022-08-21 22:56
+ * @Info : 给定一个整形数组arr,代表数值不同的纸牌排成一条线，
+ * 玩家A和玩家B依次拿走每张纸牌，
+ * 规定玩家A先拿，玩家B后拿
+ * 但是每个玩家每次只能拿走最左或最右的纸牌，
+ * 玩家A和玩家B都是绝顶聪明。返回获胜者的分数
+ */
+public class Code08_CardsInLine {
+    public static void main(String[] args) {
+
+    }
+
+}
