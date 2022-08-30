3b3d5db6ceecebc7934efc62d7f6f6bec67b5000
WXY <23144591@qq.com>
2022-08-30 22:38:58 +0800

    info 最长公共子序列

---
+++ class12/src/LongestCommonSubsequence.java
@@ -0,0 +1,35 @@
+/**
+ * @author : WXY
+ * @create : 2022-08-28 17:19
+ * @Info : 最长公共子序列
+ */
+public class LongestCommonSubsequence {
+    public static int lcse(char[] str1, char[] str2) {
+        int[][] dp = new int[str1.length][str2.length];
+        dp[0][0] = str1[0] == str2[0] ? 1 : 0;
+
+        for (int i = 1; i < str1.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], str1[i] == str2[0] ? 1 : 0);
+        }
+        for (int j = 1; j < str2.length; j++) {
+            dp[0][j] = Math.max(dp[0][j - 1], str1[0] == str2[j] ? 1 : 0);
+        }
+        for (int i = 1; i < str1.length; i++) {
+            for (int j = 1; j < str2.length; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                if (str1[i] == str2[j]) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + 1);
+                }
+            }
+        }
+        return dp[str1.length - 1][str2.length - 1];
+    }
+
+    public static void main(String[] args) {
+        String str1 = "as123dfd";
+        String str2 = "123kljnm";
+        char[] s1 = str1.toCharArray();
+        char[] s2 = str2.toCharArray();
+        System.out.println(lcse(s1, s2));
+    }
+}
